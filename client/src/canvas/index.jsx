235d62e400a219e710e4a06675b90b927161781e
hasan-tec <hasan.anas.yt@gmail.com>
2024-11-28 00:20:06 +0500

    one issue left, shirt doesnt change color

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,9 +1,15 @@
 import { Canvas } from '@react-three/fiber'
 import { Environment, Center } from '@react-three/drei';
+import { Suspense } from 'react';
 
 import Shirt from './Shirt';
 import Backdrop from './Backdrop';
 import CameraRig from './CameraRig';
+
+function ErrorFallback({ error }) {
+  console.error(error);
+  return null; // Return null to hide the error in the 3D scene
+}
 
 const CanvasModel = () => {
   return (
@@ -13,17 +19,23 @@ const CanvasModel = () => {
       gl={{ preserveDrawingBuffer: true }}
       className="w-full max-w-full h-full transition-all ease-in"
     >
-      <ambientLight intensity={0.5} />
-      <Environment preset="city" />
+      <Suspense fallback={null}>
+        <ambientLight intensity={0.5} />
+        <Environment 
+          preset="city" // Use a preset instead of loading external HDR
+          background={false} // Don't show environment as background
+        />
 
-      <CameraRig>
-        <Backdrop />
-        <Center>
-          <Shirt />
-        </Center>
-      </CameraRig>
+        <CameraRig>
+          <Backdrop />
+          <Center>
+            <Shirt />
+          </Center>
+        </CameraRig>
+      </Suspense>
     </Canvas>
   )
 }
 
 export default CanvasModel
+
